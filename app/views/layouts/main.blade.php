a66987774db6a0ecf9084a3fdc44e58dd763c37a
pitchtie14 <pitchtie@gmail.com>
2014-05-14 13:38:47 +0700

    Update main.blade.php
    add dropdown menu settings to mainpage

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -43,6 +43,7 @@
                        <ul class="nav navbar-nav pull-right">
                         <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Welcome, {{Auth::user()->username}} <b class="caret"></b></a>
                            <ul class="dropdown-menu">
+                            <li><a href="settings"><i class="icon-envelope"></i> User Settings</a></li>
                             <li><a href="#"><i class="icon-cog"></i> Preferences</a></li>
                             <li><a href="#"><i class="icon-envelope"></i> Contact Support</a></li>
                             <li class="divider"></li>
