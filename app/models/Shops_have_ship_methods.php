34c1d30270dc64dab636bbb50161c211a4ce0402
ploiro <ploiro@gmail.com>
2014-06-11 16:07:46 +0700

    BHON-50 Create function in model for create Shipping methods selector in ordering page

--- app/models/Shops_have_ship_methods.php
+++ app/models/Shops_have_ship_methods.php
@@ -2,5 +2,21 @@
 
 class Shops_have_ship_methods extends \Eloquent {
 	protected $fillable = [];
+	protected $primaryKey = 'shop_ship_id';
+  
+  public function shipping_methods() {
+  	return $this->belongsTo('Shipping_methods', 'ship_method_id');
+  }
 
+  public static function makeShipping_methods_list($shop_id) {
+  	$shipping = self::with('shipping_methods')->where('shop_id','=',$shop_id)->get();
+    $shipping_methods_selector[0] = 'เลือกรูปแบบการส่งของ';
+
+    foreach($shipping as $ship) {
+      $shop_ship_id = $ship->shop_ship_id;
+      $shipping_methods_selector[$shop_ship_id] = $ship->shipping_methods->ship_method_name." - ".$ship->rate." THB";
+    }
+    
+    return $shipping_methods_selector;
+  }
 }
